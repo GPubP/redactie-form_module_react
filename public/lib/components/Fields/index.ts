71df6d0f4455b0b6f55630977162d17b763b6aa2
Jo Smets <jo.smets@studiohyperdrive.be>
2022-01-21 07:22:03 +0100

    chore: move fieldregistry to fields file

--- public/lib/components/Fields/index.ts
+++ public/lib/components/Fields/index.ts
@@ -1,1 +1,14 @@
+import { FC } from 'react';
+
+import formRendererConnector from '../../connectors/formRenderer';
+
+import { FormSelect } from './FormSelect';
 export * from './FormSelect';
+
+formRendererConnector.api.fieldRegistry.add([
+	{
+		name: 'formReference',
+		module: 'form',
+		component: FormSelect as FC<any>,
+	},
+]);
