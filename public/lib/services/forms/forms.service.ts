ad39e3887561774cd8d32bfba73f3e853973de56
Jo Smets <jo.smets@studiohyperdrive.be>
2022-01-24 16:57:43 +0100

    chore: add site forms endpoint

--- public/lib/services/forms/forms.service.ts
+++ public/lib/services/forms/forms.service.ts
@@ -1,11 +1,16 @@
 import { apiService, parseSearchParams } from '../api';
+import { FORMS_PREFIX_URL, SITE_FORMS_PREFIX_URL } from '../api/api.service.const';
 
 import { FormsResponse } from './forms.service.types';
 
 export class FormsApiService {
-	public async getForms(query: string): Promise<FormsResponse> {
-		return await apiService
-			.get('form', {
+	public async getForms(query: string, siteId: string): Promise<FormsResponse> {
+		const path = siteId
+			? `${SITE_FORMS_PREFIX_URL}/${siteId}/forms`
+			: `${FORMS_PREFIX_URL}/forms`;
+
+		return apiService
+			.get(path, {
 				searchParams: parseSearchParams({ q: query }),
 			})
 			.json<FormsResponse>();
